81f81aa615eb9e1e12dcedc2f450f8dfc04916de
박세환 <sehwan@bagsehwan-ui-MacBookPro.local>
2026-06-21 18:50:23 +0900

    Handle private LG2 robot description topic

--- ffw_bringup/launch/ffw_lg2_leader_ai.launch.py
+++ ffw_bringup/launch/ffw_lg2_leader_ai.launch.py
@@ -66,7 +66,10 @@ def generate_launch_description():
         package='controller_manager',
         executable='ros2_control_node',
         parameters=[robot_description, robot_controllers],
-        remappings=[('robot_description', '/leader/robot_description')],
+        remappings=[
+            ('~/robot_description', '/leader/robot_description'),
+            ('robot_description', '/leader/robot_description'),
+        ],
         output='both',
     )
 
